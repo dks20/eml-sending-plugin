b6f8217b1d08aa443b0c9e1421c27862eba6100a
Deepak kumar singh <92033767+dks20@users.noreply.github.com>
2023-12-19 12:29:04 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,6 +1,12 @@
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.action === 'scrapeContent') {
-      var content = document.body.textContent;
-      chrome.runtime.sendMessage({ action: 'downloadContent', content: content });
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.action === 'scrapeHTML') {
+      const legacyThreadId = document.querySelector('[data-legacy-thread-id]');
+      const result = legacyThreadId ? legacyThreadId.getAttribute('data-legacy-thread-id') : 'Not found';
+      const url1 = "https://mail.google.com/mail/u/0?view=att&th="
+      const url2 = "&attid=0&disp=comp&safe=1&zw"
+      let url = url1.concat(result + url2);
+      sendResponse({ data: url });
     }
   });
+  
+
